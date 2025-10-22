0d92a1ab6b9d2fd7894f4a7473fd3df9c45f9877
kunstewi <kan.ai35423@gmail.com>
2025-10-22 19:43:39 +0530

    improved folder structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { UsersModule } from './modules/users/users.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
